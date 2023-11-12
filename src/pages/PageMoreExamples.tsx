7a43310967130a29c2bd9d1de1154e0ca16a626f
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-12 18:10:28 +0100

    gradient transparent doesn't work

--- src/pages/PageMoreExamples.tsx
+++ src/pages/PageMoreExamples.tsx
@@ -4,12 +4,14 @@ export const PageMoreExamples = () => {
 	return (
 		<>
 			<section>
-				<h2 className="text-3xl mb-3">
-				four Tailwind mobile sizes	
-				</h2>
-				<div className="block sm:hidden md:hidden lg:hidden">smartphone</div>
+				<h2 className="text-3xl mb-3">four Tailwind mobile sizes</h2>
+				<div className="block sm:hidden md:hidden lg:hidden">
+					smartphone
+				</div>
 				<div className="hidden sm:block md:hidden lg:hidden">small</div>
-				<div className="hidden sm:hidden md:block lg:hidden">medium</div>
+				<div className="hidden sm:hidden md:block lg:hidden">
+					medium
+				</div>
 				<div className="hidden sm:hidden md:hidden lg:block">large</div>
 			</section>
 			<section className="w-full">
@@ -19,15 +21,17 @@ export const PageMoreExamples = () => {
 				<div className="flex gap-3 flex-wrap w-full">
 					{employees.map((employee) => {
 						return (
-							<div className="w-full sm:w-auto"> 
-								<img
-									className="w-full sm:w-40 md:w-40 lg:w-80 rounded-lg"
-									src={`images/employees/employee_${employee.employeeID}.jpg`}
-								/>
-								<p>
-									{employee.firstName} {employee.lastName}
-								</p>
-							</div>
+							<>
+								<div className="w-full sm:w-40 md:w-40 lg:w-80 rounded-lg relative">
+									<p className="absolute left-0 p-4 top-0 bg-gradient-to-tr from-white-to-transparent w-full">
+										{employee.firstName} {employee.lastName}
+									</p>
+									<img
+										className="w-full"
+										src={`images/employees/employee_${employee.employeeID}.jpg`}
+									/>
+								</div>
+							</>
 						);
 					})}
 				</div>
